a6715a6105e2348ec9ebf937b59c4b173f0910d1
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-09 18:46:54 +0330

    Add first QA

---
+++ 1/1.js
@@ -0,0 +1,7 @@
+// Practical:
+// Write a function that takes two numbers as parameters and returns their sum. Use let for local variables, and const for the function parameters. Example input: sum(5, 7); & Expected output: 12
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 7));
